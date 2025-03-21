7a305d0b5237b75b084360996cfe7a3bbff6f84e
Melanie <melanie@t-data.com>
2025-03-21 00:05:25 +0000

    Kconfig magic and changes to handlers, fix crash bug

--- AppFramework.cpp
+++ AppFramework.cpp
@@ -3,50 +3,115 @@
 //
 
 #include <esp_wifi.h>
+#include <esp_mac.h>
 #include "AppFramework.h"
+#include "nvs_flash.h"
+#include "CPPNVS.h"
 
-AppFramework::AppFramework(WiFi &wifi)
+using namespace std;
+
+AppFramework::AppFramework(WiFi *wifi)
     :m_wifi(wifi)
+{}
+
+void AppFramework::init()
 {
+#ifdef CONFIG_USE_SETUP_WEB_INTERFACE
+    m_hostname = generateHostname();
+#endif
+
+    NVS nvs("wifi");
+
+    m_wifi->setWifiEventHandler(this);
+
+    if (m_ssid.empty())
+    {
+        if (nvs.get("ssid", &m_ssid))
+            m_ssid.clear();
+        if (nvs.get("password", &m_password))
+            m_password.clear();
+    }
+
+    if (!m_ssid.empty())
+    {
+        m_wifi->connectSTA(m_ssid, m_password);
+    }
+#ifdef CONFIG_USE_SETUP_WEB_INTERFACE
+    else
+    {
+        auto auth = WIFI_AUTH_WPA2_WPA3_PSK;
+        if (m_password.empty())
+            auth = WIFI_AUTH_OPEN;
+
+        m_wifi->startAP(m_hostname, m_password, auth);
+    }
+#endif
+
+}
+
+string AppFramework::generateHostname()
+{
+    uint8_t chipid[6];
+    esp_read_mac(chipid, ESP_MAC_WIFI_STA);
+    static char hostname[32] = CONFIG_AP_MODE_HOSTNAME_PREFIX;
+    hostname[sizeof(hostname) - 1] = 0;
+    snprintf(hostname + strlen(hostname), sizeof(hostname) - strlen(hostname) - 1, "_%02x%02x%02x", chipid[3], chipid[4], chipid[5]);
+
+    return {hostname};
+}
+
+esp_err_t AppFramework::staStart()
+{
+    return ESP_OK;
+}
+
+esp_err_t AppFramework::staStop()
+{
+    if (m_staActive)
+    {
+        m_staActive = false;
+        if (m_handler)
+            m_handler->staStopped();
+    }
+    return ESP_OK;
 }
 
 esp_err_t AppFramework::staDisconnected(wifi_event_sta_disconnected_t *info)
 {
     if (m_staActive)
         esp_wifi_connect();
+    return ESP_OK;
 }
 
 esp_err_t AppFramework::staGotIp(ip_event_got_ip_t *info)
 {
-
+    if (!m_staActive)
+    {
+        m_staActive = true;
+        if (m_handler)
+            m_handler->staActive();
+    }
+    return ESP_OK;
 }
 
 esp_err_t AppFramework::apStart()
 {
-    return WiFiEventHandler::apStart();
+    if (!m_apActive)
+    {
+        m_apActive = true;
+        if (m_handler)
+            m_handler->apActive();
+    }
+    return ESP_OK;
 }
 
 esp_err_t AppFramework::apStop()
 {
-    return WiFiEventHandler::apStop();
-}
-
-void AppFramework::init()
-{
-
-}
-
-void AppFramework::init(bool enableApConfiguration)
-{
-    this->enableApConfiguration(enableApConfiguration);
-
-    init();
-}
-
-void AppFramework::init(bool enableApConfiguration, std::string spiffsPartitionName)
-{
-    this->enableApConfiguration(enableApConfiguration);
-    setSpiffsPartitionName(spiffsPartitionName);
-
-    init();
+    if (m_apActive)
+    {
+        m_apActive = false;
+        if (m_handler)
+            m_handler->apStopped();
+    }
+    return ESP_OK;
 }
--- AppFramework.h
+++ AppFramework.h
@@ -9,7 +9,10 @@
 #include <utility>
 #include "WiFi.h"
 
-class AppFrameworkHandler {
+#define APP_RUN(x) extern "C" { void app_main() { x.run(); }}
+
+class AppFrameworkHandler
+{
 public:
     virtual void apActive() {};
     virtual void apStopped() {};
@@ -20,29 +23,35 @@ public:
 class AppFramework : public WiFiEventHandler
 {
 public:
-    explicit AppFramework(WiFi& wifi);
-
-    inline void enableApConfiguration(bool ap) { m_useApConfiguration = ap; };
-    inline void setSpiffsPartitionName(std::string spiffs) { m_spiffsPartitionName = std::move(spiffs); };
+    explicit AppFramework(WiFi *wifi);
     void init();
-    void init(bool enableApConfiguration);
-    void init(bool enableApConfiguration, std::string spiffsPartitionName);
     inline void setHandler(AppFrameworkHandler *handler) { m_handler = handler; };
+    inline void setAuthentication(std::string ssid, std::string password) { m_ssid = std::move(ssid); m_password = std::move(password); };
+#ifdef CONFIG_USE_SETUP_WEB_INTERFACE
+    inline std::string hostname() { return m_hostname; };
+#endif
 
+    esp_err_t staStart() override;
+    esp_err_t staStop() override;
     esp_err_t staDisconnected(wifi_event_sta_disconnected_t *info) override;
     esp_err_t staGotIp(ip_event_got_ip_t *info) override;
     esp_err_t apStart() override;
     esp_err_t apStop() override;
 
 private:
+    std::string generateHostname();
+
     bool m_staActive = false;
-    bool apActive = false;
+    bool m_apActive = false;
     std::string m_ssid = {};
     std::string m_password = {};
-    bool m_useApConfiguration = false;
-    std::string m_spiffsPartitionName = "spiffs";
-    WiFi& m_wifi;
-    AppFrameworkHandler *m_handler;
+#ifdef CONFIG_USE_SETUP_WEB_INTERFACE
+    std::string m_hostname;
+#endif
+    std::string m_spiffsPartitionName = CONFIG_WEB_INTERFACE_SPIFFS_PARTITION_NAME;
+    WiFi *m_wifi = nullptr;
+    AppFrameworkHandler *m_handler = nullptr;
+
 };
 
 
